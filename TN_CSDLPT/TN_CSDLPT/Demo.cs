a2dd7494ae50542266dd4161e0a1e5776261860d
sonnlh.deephub <sonnlh@deephub.vn>
2020-07-26 18:19:05 +0700

    update: Demo

---
+++ TN_CSDLPT/TN_CSDLPT/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace TN_CSDLPT
+{
+    public partial class Demo : DevExpress.XtraEditors.XtraForm
+    {
+        public Demo()
+        {
+            InitializeComponent();
+        }
+    }
+}
